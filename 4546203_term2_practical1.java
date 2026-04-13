261554248da36aadae88014ac0914f3bc7e70e5a
Samantha-2461 <siwelasamantha49@gmail.com>
2026-04-13 14:46:24 +0200

    Update 4546203_term2_practical1.java

--- 4546203_term2_practical1.java
+++ 4546203_term2_practical1.java
@@ -1,6 +1,9 @@
-class BinarTreeNode<E>
-  {
+public class BinaryTreeNode<E> {
+  private E content;
     E value;
     BinaryTreeNode<E> left;
     BinaryTreeNode<E> right;
+  public void set (E content) {this.content=content}
+    }
   }
+
