8d7550aad324d928b7b3ad636028f85250acf230
Samantha-2461 <siwelasamantha49@gmail.com>
2026-04-13 15:28:43 +0200

    Update 4546203_term2_pratical1_part2.java

--- 4546203_term2_pratical1_part2.java
+++ 4546203_term2_pratical1_part2.java
@@ -1,5 +1,26 @@
 public class BinaryTree<E> {
-  private E value;
-    int size;
-    BinaryTreeNode<E> root;
+  private int size;
+  private BinaryTree<E> root;
+
+  // default constructor 
+  public class BinaryTree(T value){
+  this.size=size;
+  this.root=null;
+  
+}
+// the load constructor 
+public class BinaryTree( ) {
+        this.size = size;
+        this.root = null;
+       
+    }
+// Getter method 
+public int getSize() { return size;}
+public BinaryTree getoot() { return root;}
+
+//Setter method
+public void setSize (int Size) {this.size = size;}
+public void setRoot (BinaryTree Left) {this.root = root;}
+
+
   }
